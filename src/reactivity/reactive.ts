55c56d662db71d726d42c9893de5d786686ba48d
cubsdiary <yangjingpuyu@aliyun.com>
2022-04-11 22:50:06 +0800

    feat: 实现 isReactive 和 isReadonly

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,5 +1,15 @@
 import { mutableHandlers, readonlyHandlers } from './baseHandles';
 
+export const enum ReactiveFlags {
+    IS_REACTIVE = '__v_isReactive',
+    IS_READONLY = '__v_isReadonly',
+}
+
+export interface Target {
+    [ReactiveFlags.IS_REACTIVE]?: boolean;
+    [ReactiveFlags.IS_READONLY]?: boolean;
+}
+
 export function reactive(target: object) {
     return createReactiveObject(target, false, mutableHandlers);
 }
@@ -10,4 +20,12 @@ export function readonly(target: object) {
 
 function createReactiveObject(target: object, isReadonly = false, baseHandlers: ProxyHandler<any>) {
     return new Proxy(target, baseHandlers);
+}
+
+export function isReadonly(value: unknown): boolean {
+    return !!(value && (value as Target)[ReactiveFlags.IS_READONLY]);
+}
+
+export function isReactive(value: unknown): boolean {
+    return !!(value && (value as Target)[ReactiveFlags.IS_REACTIVE]);
 }
--- src/reactivity/tests/reactive.spec.ts
+++ src/reactivity/tests/reactive.spec.ts
@@ -1,4 +1,4 @@
-import { reactive } from '../reactive';
+import { isReactive, reactive } from '../reactive';
 
 describe('reactive', () => {
     it('happy path', () => {
@@ -7,5 +7,8 @@ describe('reactive', () => {
 
         expect(observed).not.toBe(original);
         expect(observed.foo).toBe(1);
+
+        expect(isReactive(observed)).toBe(true);
+        expect(isReactive(original)).toBe(false);
     });
 });
