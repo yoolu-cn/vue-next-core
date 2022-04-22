fe00b26f711272c18421f2d1e55b4bc41bc26610
cubsdiary <yangjingpuyu@aliyun.com>
2022-04-23 01:28:40 +0800

    feat: 实现 Fragment 和 Text 类型节点

--- src/runtime-core/helpers/renderSlot.ts
+++ src/runtime-core/helpers/renderSlot.ts
@@ -1,12 +1,12 @@
 import { isFunction } from '../../shared';
 import { Data } from '../component';
-import { createVNode } from '../vnode';
+import { createVNode, Fragment } from '../vnode';
 
 export function renderSlot(slots: any, name: string, props: Data) {
     const slot = slots[name];
     if (slot) {
         if (isFunction(slot)) {
-            return createVNode('div', {}, slot(props));
+            return createVNode(Fragment, {}, slot(props));
         }
     }
 }
